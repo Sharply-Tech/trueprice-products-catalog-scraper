b71e91aa3095c1a20f1eccf3124cb93ea6dd147c
Cosmin Stoian <cosmin@bceng.tech>
2020-10-07 23:35:11 +0300

    Implemented product title extracting from page of products in category. Still some problems to solve. It doesn

---
+++ README.md
@@ -0,0 +1,8 @@
+## About
+
+This service is used to automatically register products in the system from a reliable source, such as
+[https://emag.ro](https://emag.ro)
+
+## Project Configuration
+
+The project uses [PlayWright](https://playwright.dev/) to scrape for products.
---
+++ src/app.ts
@@ -0,0 +1,116 @@
+import {chromium} from "playwright";
+
+
+enum ProductAvailabilityType {
+    AVAILABLE,
+    UNAVAILABLE,
+    PRE_ORDER
+}
+
+interface ProductAvailability {
+    availability: ProductAvailabilityType,
+    itemsLeftOnStock: null
+}
+
+interface Product {
+    title: string,
+    url: string,
+    availability: ProductAvailability
+}
+
+const CATEGORIES = [
+    'telefoane-mobile',
+    'televizoare',
+    'laptopuri'
+]
+
+const getProductsFromCategory = async (category: string): Promise<any> => {
+    const browser = await chromium.launch({headless: false});
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    const navigationPromise = page.waitForNavigation();
+
+    await page.goto(resolveCategoryProductsLink(category));
+    await page.setViewportSize({width: 1278, height: 1287});
+    await navigationPromise;
+
+    // Identify the number of products on page and number of total products in category\
+    // Total number of products in category
+    const totalProductsHandler = await page.waitForSelector('.listing-panel > .listing-panel-footer > .row > .col-lg-3 > .control-label')
+    const productsPaginationText = await totalProductsHandler.innerText();
+
+    // tslint:disable-next-line:radix
+    const totalProducts = parseInt(productsPaginationText
+        .substring(productsPaginationText.indexOf('din') + 4)
+        .replace('de produse', '')
+        .replace(' ', ''))
+    console.log(`Total products for category ${category} is ${totalProducts}`)
+
+    const [currentPageStart, currentPageEnd] = productsPaginationText
+        .substring(0, productsPaginationText.indexOf('din'))
+        .split('-')
+        .map(str => parseInt(str, 10));
+
+    const currentPageSize = currentPageEnd - currentPageStart + 1;
+    console.log(`Current page start ${currentPageStart}, current page end ${currentPageEnd}, current page size ${currentPageSize}`)
+
+    const isFirstPage: boolean = currentPageStart === 1;
+    const isLastPage: boolean = currentPageEnd === totalProducts;
+
+    const products = []
+
+    let productIndex = 1;
+    while(products.length < currentPageSize &&
+            productIndex <= 1.1 * currentPageEnd) { // abatere 10%
+
+        try {
+            const productTitleHandler = await page.waitForSelector(`.card-item:nth-child(${productIndex}) > .card > .card-section-wrapper > .card-section-mid > .card-body`, {timeout: 50})
+            const productTitle = await productTitleHandler.innerText();
+            products.push(productTitle)
+            console.log('Scanned product: ' + productTitle)
+        } catch {
+            console.log(category + ' -> skipping for index ' + productIndex)
+        }
+        productIndex++
+    }
+
+    console.log(`Found ${products.length} products for ${category}`)
+    // Select first product's title
+
+    // console.log('')
+    /*
+    await page.waitForSelector('.card-item:nth-child(13) > .card > .card-section-wrapper > .card-section-mid > .card-body')
+    await page.click('.card-item:nth-child(13) > .card > .card-section-wrapper > .card-section-mid > .card-body')
+
+    await page.waitForSelector('.card-item:nth-child(15) > .card > .card-section-wrapper > .card-section-mid > .card-body')
+    await page.click('.card-item:nth-child(15) > .card > .card-section-wrapper > .card-section-mid > .card-body')
+
+
+    await page.waitForSelector('.card-item:nth-child(1) > .card > .card-section-wrapper > .card-section-btm > .card-body > .product-stock-status')
+    await page.click('.card-item:nth-child(1) > .card > .card-section-wrapper > .card-section-btm > .card-body > .product-stock-status')
+
+    await page.waitForSelector('.card-item:nth-child(1) > .card > .card-section-wrapper > .card-section-btm > .card-body > .pricing-old_preserve-space > .product-old-price')
+    await page.click('.card-item:nth-child(1) > .card > .card-section-wrapper > .card-section-btm > .card-body > .pricing-old_preserve-space > .product-old-price')
+
+    await page.waitForSelector('.card-item:nth-child(1) > .card > .card-section-wrapper > .card-section-btm > .card-body > .pricing-old_preserve-space > .product-new-price')
+    await page.click('.card-item:nth-child(1) > .card > .card-section-wrapper > .card-section-btm > .card-body > .pricing-old_preserve-space > .product-new-price')
+
+    await page.waitForSelector('.container > .clearfix > .page-container > #card_grid > .card-item:nth-child(2)')
+    await page.click('.container > .clearfix > .page-container > #card_grid > .card-item:nth-child(2)')
+    */
+    await browser.close()
+}
+
+const resolveCategoryProductsLink = (category: string) => {
+    return `https://www.emag.ro/${category}/c`
+}
+
+(async () => {
+    const startTime = Date.now();
+
+    const promises = CATEGORIES.map((category) => getProductsFromCategory(category));
+    await Promise.all(promises);
+    const timeEffortInMillis = Date.now() - startTime;
+    console.log(`Time effort:  ${timeEffortInMillis} millis`);
+})()
