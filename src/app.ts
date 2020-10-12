736851c6bf82f9c98d8f871dd8ec3d84000e755c
Cosmin Stoian <cosmin@bceng.tech>
2020-10-12 21:07:42 +0300

    Changed the handler for product title to extract property from the div; added handler for product availability (NOT WORKING); removed some short set timeouts.

--- src/app.ts
+++ src/app.ts
@@ -1,106 +1,205 @@
-import {chromium} from "playwright";
-
+import { chromium } from "playwright";
 
 enum ProductAvailabilityType {
     AVAILABLE,
+    LIMITED,
     UNAVAILABLE,
-    PRE_ORDER
+    PRE_ORDER,
 }
 
 interface ProductAvailability {
-    availability: ProductAvailabilityType,
-    itemsLeftOnStock: null
+    availability: ProductAvailabilityType;
+    itemsLeftOnStock?: number;
+    estimatedDeliveryDays?: number;
 }
 
 interface Product {
-    title: string,
-    url?: string,
-    availability?: ProductAvailability,
-    oldPrice?: number,
-    price: number
+    title: string;
+    url?: string;
+    availability?: ProductAvailability;
+    oldPrice?: number;
+    price: number;
 }
 
 const CATEGORIES = [
-    'telefoane-mobile',
-    'televizoare',
-    'laptopuri'
-]
+    "telefoane-mobile",
+    "televizoare",
+    "laptopuri",
+    "smartwatch",
+];
+
+const getProductAvailability = (stockInfo: string) : ProductAvailability => {
+    if (stockInfo == null) {
+        return null;
+    }
+   
+    stockInfo = stockInfo.toLowerCase().replace('ă', 'a').replace('â', 'a').replace('î', 'i').replace('ș', 's').replace('ț', 't')
+    if (stockInfo === 'instoc') {
+        return {
+            availability: ProductAvailabilityType.AVAILABLE
+        }
+    }
+
+    if (stockInfo === 'indisponibil') {
+        return {
+            availability: ProductAvailabilityType.UNAVAILABLE
+        }
+    }
+
+    if (stockInfo.startsWith('livrarein')) {
+        return {
+            availability: ProductAvailabilityType.AVAILABLE,
+            estimatedDeliveryDays: parseInt(stockInfo.replace('livrarein', '').replace('zile', ''))
+        }
+    }
+
+    if (stockInfo.startsWith('ultimele')) {
+        return {
+            availability: ProductAvailabilityType.AVAILABLE,
+            itemsLeftOnStock: parseInt(stockInfo.replace('ultimele', '').replace('produse', ''))
+        }
+    }
+
+    if (stockInfo === 'ultimulprodusinstoc') {
+        return {
+            availability: ProductAvailabilityType.AVAILABLE,
+            itemsLeftOnStock: 1
+        }
+    }
+
+    if (stockInfo === 'stoclimitat') {
+        return {
+            availability: ProductAvailabilityType.LIMITED
+        }
+    }
+
+    if (stockInfo.startsWith('precomanda')) {
+        return {
+            availability: ProductAvailabilityType.PRE_ORDER
+        }
+    }
+
+    return null
+}
 
 const getProductsFromCategory = async (category: string): Promise<any> => {
-    const browser = await chromium.launch({headless: false});
+    const browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
 
     const navigationPromise = page.waitForNavigation();
 
     await page.goto(resolveCategoryProductsLink(category));
-    await page.setViewportSize({width: 1278, height: 1287});
+    await page.setViewportSize({ width: 1278, height: 1287 });
     await navigationPromise;
 
     // Identify the number of products on page and number of total products in category\
     // Total number of products in category
-    const totalProductsHandler = await page.waitForSelector('.listing-panel > .listing-panel-footer > .row > .col-lg-3 > .control-label')
+    const totalProductsHandler = await page.waitForSelector(
+        ".listing-panel > .listing-panel-footer > .row > .col-lg-3 > .control-label"
+    );
     const productsPaginationText = await totalProductsHandler.innerText();
 
     // tslint:disable-next-line:radix
-    const totalProducts = parseInt(productsPaginationText
-        .substring(productsPaginationText.indexOf('din') + 4)
-        .replace('de produse', '')
-        .replace(' ', ''))
-    console.log(`Total products for category ${category} is ${totalProducts}`)
+    const totalProducts = parseInt(
+        productsPaginationText
+            .substring(productsPaginationText.indexOf("din") + 4)
+            .replace("de produse", "")
+            .replace(" ", "")
+    );
+    console.log(`Total products for category ${category} is ${totalProducts}`);
 
     const [currentPageStart, currentPageEnd] = productsPaginationText
-        .substring(0, productsPaginationText.indexOf('din'))
-        .split('-')
-        .map(str => parseInt(str, 10));
+        .substring(0, productsPaginationText.indexOf("din"))
+        .split("-")
+        .map((str) => parseInt(str, 10));
 
     const currentPageSize = currentPageEnd - currentPageStart + 1;
-    console.log(`Current page start ${currentPageStart}, current page end ${currentPageEnd}, current page size ${currentPageSize}`)
+    console.log(
+        `Current page start ${currentPageStart}, current page end ${currentPageEnd}, current page size ${currentPageSize}`
+    );
 
     const isFirstPage: boolean = currentPageStart === 1;
     const isLastPage: boolean = currentPageEnd === totalProducts;
 
-    const products: Product[] = []
+    const products: Product[] = [];
 
     let productIndex = 1;
-    while(products.length < currentPageSize &&
-            productIndex <= 1.1 * currentPageEnd) { // abatere 10%
+    while (
+        products.length < currentPageSize &&
+        productIndex <= 1.1 * currentPageEnd
+    ) {
+        // abatere 10%
 
         try {
-            const productTitleHandler = await page.waitForSelector(`.card-item:nth-child(${productIndex}) > .card > .card-section-wrapper > .card-section-mid > .card-body`, {timeout: 50})
-            const productTitle = await productTitleHandler.innerText();
-            // products.push(productTitle)
+            // const productTitleHandler = await page.waitForSelector(
+            //     `.card-item:nth-child(${productIndex}) > .card > .card-section-wrapper > .card-section-mid > .card-body`,
+            //     { timeout: 50 }
+            // );
+            // const productTitle = await productTitleHandler.innerText();
+            
+            // TODO: De vazut care dintre metodele pentru titlu este mai performanta
 
-            const oldPriceHandler =     await page.waitForSelector(`.card-item:nth-child(${productIndex}) > .card > .card-section-wrapper > .card-section-btm > .card-body > .pricing-old_preserve-space > .product-old-price`)
+            const productTitleHandler = await page.waitForSelector(
+                `.card-item:nth-child(${productIndex})`
+            );
+
+            const productTitle = await productTitleHandler.getAttribute('data-name')
+
+            
+            const productUrlHandler = await (await page.waitForSelector(`.card-item:nth-child(${productIndex}) > .card > .card-section-wrapper > .card-section-top`)).waitForSelector(`.js-product-url`)
+
+            const productUrl = await productUrlHandler.getAttribute('href')
+
+            const oldPriceHandler = await page.waitForSelector(
+                `.card-item:nth-child(${productIndex}) > .card > .card-section-wrapper > .card-section-btm > .card-body > .pricing-old_preserve-space > .product-old-price`
+            );
             const oldPriceStr = await oldPriceHandler.innerText();
 
-            const oldPrice = oldPriceStr == null || oldPriceStr.trim() === '' ? null :
-                parseFloat(oldPriceStr.substring(0, oldPriceStr.indexOf('Lei'))
-                    .toLowerCase()
-                    .replace('lei', '')
-                    .replace('.', ''))/100;
+            const oldPrice =
+                oldPriceStr == null || oldPriceStr.trim() === ""
+                    ? null
+                    : parseFloat(
+                          oldPriceStr
+                              .substring(0, oldPriceStr.indexOf("Lei"))
+                              .toLowerCase()
+                              .replace("lei", "")
+                              .replace(".", "")
+                      ) / 100;
 
-            const priceHandler = await page.waitForSelector(`.card-item:nth-child(${productIndex}) > .card > .card-section-wrapper > .card-section-btm > .card-body > .pricing-old_preserve-space > .product-new-price`)
+            const priceHandler = await page.waitForSelector(
+                `.card-item:nth-child(${productIndex}) > .card > .card-section-wrapper > .card-section-btm > .card-body > .pricing-old_preserve-space > .product-new-price`
+            );
             const priceStr: string = await priceHandler.innerText();
-            const price = parseFloat(priceStr.toLowerCase().toLowerCase()
-                .replace('lei', '')
-                .replace('.', ''))/100
+            const price =
+                parseFloat(
+                    priceStr
+                        .toLowerCase()
+                        .toLowerCase()
+                        .replace("lei", "")
+                        .replace(".", "")
+                ) / 100;
+
+            const productAvailabilityHandler = await page.waitForSelector(`.card-item:nth-child(${productIndex}) > .card > .card-section-wrapper > .card-section-btm > .card-body > .product-stock-status`)
+            const productAvailability = getProductAvailability(await productAvailabilityHandler.innerText())
 
             products.push({
                 title: productTitle,
                 price,
-                oldPrice
-            })
+                oldPrice,
+                url: productUrl,
+                availability: productAvailability
+            });
             // console.log('Scanned product: ' + productTitle)
-        } catch {
-            console.log(category + ' -> skipping for index ' + productIndex)
+        } catch (ex) {
+            console.error(category + " -> skipping for index " + productIndex + ", ex: ", ex);
         }
-        productIndex++
+        productIndex++;
     }
 
-    console.log(`Found ${products.length} products for ${category}`)
-    console.log(`${category} products: `)
-    console.log(JSON.stringify(products))
+    console.log(`Found ${products.length} products for ${category}`);
+    console.log(`${category} products: `);
+    console.log(JSON.stringify(products));
 
     // Select first product's title
 
@@ -125,18 +224,20 @@ const getProductsFromCategory = async (category: string): Promise<any> => {
     await page.waitForSelector('.container > .clearfix > .page-container > #card_grid > .card-item:nth-child(2)')
     await page.click('.container > .clearfix > .page-container > #card_grid > .card-item:nth-child(2)')
     */
-    await browser.close()
-}
+    await browser.close();
+};
 
 const resolveCategoryProductsLink = (category: string) => {
-    return `https://www.emag.ro/${category}/c`
-}
+    return `https://www.emag.ro/${category}/c`;
+};
 
 (async () => {
     const startTime = Date.now();
 
-    const promises = CATEGORIES.map((category) => getProductsFromCategory(category));
+    const promises = CATEGORIES.map((category) =>
+        getProductsFromCategory(category)
+    );
     await Promise.all(promises);
     const timeEffortInMillis = Date.now() - startTime;
     console.log(`Time effort:  ${timeEffortInMillis} millis`);
-})()
+})();
